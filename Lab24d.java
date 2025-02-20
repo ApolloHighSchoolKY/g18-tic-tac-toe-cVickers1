f4682ff6ec1300154a1511d1e4761aecef82d850
cVickers1 <cameron.vickers@stu.daviess.kyschools.us>
2025-02-20 14:30:34 +0000

    yeowch

--- Lab24d.java
+++ Lab24d.java
@@ -15,8 +15,20 @@ public class Lab24d
 		Scanner file = new Scanner (new File("lab24d.dat"));
 
 
+		int x =(file.nextInt());
+		file.nextLine();
 
+		TicTacToe game = new TicTacToe(file.nextLine());
+		System.out.println(game);
+		//read how many times you need to read
 
+		//tic tac toe only wants file.nextLine()
+
+		//make a tictactoe object for each game in the file
+
+		//print the game(tictactoe object)
+	
+		//print the winner
 
 
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -13,22 +13,40 @@ public class TicTacToe
 
 	public TicTacToe()
 	{
-
-
+		mat[0][0] = 
+		mat[0][1] =
+		mat[0][2] =
+		mat[1][0] =
+		mat[2][0] =
+		mat[1][1] =
+		mat[2][2] =
+		mat[2][1] =
+		mat[1][2] =
 	}
 
 	public TicTacToe(String game)
 	{
-
-
-
+	//row major order happens here 
+	for (int row = 0; row < mat.length; row++)
+	{
+		for(int col = 0; col<mat[row].length; col++)
+		{
+			if()
+		}
+	}
 
 	}
 
 	public String getWinner()
 	{
+		//9 level else if
 
 
+
+
+
+		/*  X X X
+		    O O O  */
 
 
 
